fe4f034ec32b34baf518797216fc5c7b87f388c5
yang <yxiangxiao@163.com>
2013-10-15 21:49:59 +0800

    add output class

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -1,10 +1,25 @@
+class Output
+  def messages
+    @messages ||= []
+  end
+  def  puts(message)
+    messages << message
+  end
+end
+def output
+  @output ||= Output.new
+end
+
 Given(/^I not yet play game$/) do
 end
 
 When(/^i start a new game$/) do
-  Codebreaker::Game.new.start 
+  #  Codebreaker::Game.new.start
+  game=Codebreaker::Game.new(output)
+  game.start
 end
 
 Then(/^I should see "(.*?)"$/) do |message|
+  output.messages.should include(message)
 end
 
